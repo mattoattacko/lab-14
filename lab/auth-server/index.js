43cfa0fcdf7e5693411dd2c201f07bb1d2372760
Matto <matt.mcquain@gmail.com>
2018-11-10 17:55:55 -0800

    mostly finished

--- lab/auth-server/index.js
+++ lab/auth-server/index.js
@@ -9,7 +9,7 @@ require('babel-register');
 const mongoose = require('mongoose');
 const options = {
   useNewUrlParser:true,
-  useCreateIndex: true
+  useCreateIndex: true,
 };
 mongoose.connect(process.env.MONGODB_URI, options);
 
